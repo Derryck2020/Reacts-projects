2afadcf9ffb526af35afd9f846cbc8ea9390866d
Derryck <oseiagyemanb@gmail.com>
2023-03-25 12:21:15 +0100

    initial commit of files

---
+++ src/data.js
@@ -0,0 +1,37 @@
+export default [
+   {
+      id: 1,
+      name: 'Bertie Yates',
+      age: 29,
+      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
+      occupation: 'Human Resource Manager',
+   },
+   {
+      id: 2,
+      name: 'Hester Hogan',
+      age: 32,
+      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
+      occupation: 'Public Relation Officer',
+   },
+   {
+      id: 3,
+      name: 'Larry Little',
+      age: 27,
+      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+      occupation: 'Frontend Engineer',
+   },
+   {
+      id: 4,
+      name: 'Sean Walsh',
+      age: 33,
+      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+      occupation: 'CTO',
+   },
+   {
+      id: 5,
+      name: 'Lola Gardner',
+      age: 38,
+      image: 'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+      occupation: 'Axuillary MD',
+   },
+];
